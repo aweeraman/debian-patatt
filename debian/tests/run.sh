e31fa94f22a31adb409e992b83d5d18b57554f33
Anuradha Weeraman <anuradha@debian.org>
2021-09-26 19:08:12 +0530

    Added autopkgtests for pgp

--- debian/tests/run.sh
+++ debian/tests/run.sh
@@ -4,13 +4,21 @@ echo "Copying keys needed for tests..."
 mkdir -p $HOME/.local/share/patatt/public/ || true
 cp -r .keys/* $HOME/.local/share/patatt/public/
 
-echo "Running positive test..."
+echo "Running ed25519 positive test..."
 patatt validate samples/ed25519-signed.txt
 
 [ $? = 0 ] || exit 1
 
-echo "Running negative test..."
+echo "Running ed25519 negative test..."
 patatt validate debian/tests/ed25519-sign-fail.txt
+
+echo "Running pgp positive test..."
+patatt validate samples/pgp-signed.txt
+
+[ $? = 0 ] || exit 1
+
+echo "Running pgp negative test..."
+patatt validate debian/tests/pgp-sign-fail.txt
 
 [ $? != 0 ] || exit 1
 
